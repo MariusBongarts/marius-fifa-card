a866fb31577a520bb4481b1f4e5cc8ff09a4e81e
Marius Bongarts <mb709027@fh-muenster.de>
2019-06-29 10:54:37 +0200

    Implemented futhead design

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -27,31 +27,105 @@ class AppComponent extends LitElement {
   playerName: string = 'Messi';
   @property()
   imageUrl: string = 'https://thispersondoesnotexist.com/image';
+  @property()
+  position: string = 'ST';
 
   render() {
     return html`
-          <div class="card">
-            <div class="cardBody">
-              <span class="overall">${this.overall}</span>
-              <img src=${this.imageUrl}>
-            </div>
+<div class="playercard  fut19 card-large  nif  gold text-center center-block player-detail-card " style="margin: auto; ">
 
-            <div class="stats">
-              <div class="playerName">
-                ${this.playerName}
-              </div>
-              <div class="statsLeft">
-                <p>${this.pac} PAC</p>
-                <p>${this.sho} SHO</p>
-                <p>${this.pas} PAS</p>
-              </div>
-              <div class="statsRight">
-                <p>${this.dri} DRI</p>
-                <p>${this.def} DEF</p>
-                <p>${this.phy} PHY</p>
-              </div>
-            </div>
-          </div>
+
+  <span class="sheen"></span>
+
+  <div class="playercard-rating">
+    ${this.overall}
+  </div>
+
+  <div class="playercard-name">
+    ${this.playerName}
+  </div>
+
+  <div class="playercard-position">
+
+    ${this.position}
+
+  </div>
+
+  <div class="playercard-nation">
+    <img height="18" width="30" alt="Germany" src="https://futhead.cursecdn.com/static/img/19/nations/21.png" data-src="https://futhead.cursecdn.com/static/img/19/nations/21.png"
+      class="loaded" data-was-processed="true">
+  </div>
+
+  <div class="playercard-club">
+    <img height="30" width="30" alt="Werder Bremen" src="https://cdn.fupa.net/club/png/50x50/th7P2b155brFxjMRmYOHG5G6VB27U02fIeFR5xM1"
+      data-src="https://cdn.fupa.net/club/png/50x50/th7P2b155brFxjMRmYOHG5G6VB27U02fIeFR5xM1" class="loaded" data-was-processed="true">
+  </div>
+
+  <div class="playercard-picture">
+    <img style="border-radius: 50px" height="135" width="125" alt="Max Kruse 82"
+    src="https://cdn.fupa.net/player/jpeg/256x320/iVAUsxKAm4fCgRD0ldjSZ9M0MX7eYBnmZtoJKNLy"
+      data-src="https://futhead.cursecdn.com/static/img/19/players/183574.png" class="loaded" data-was-processed="true">
+  </div>
+
+
+
+
+  <div class="playercard-extra">
+
+
+    <div class="playercard-skill-move">
+      4<span class="icon icon-star"></span>SM
+    </div>
+
+
+    <div class="playercard-weak-foot">
+      4<span class="icon icon-star"></span>WF
+    </div>
+
+
+    <div class="playercard-feature-price feature-prices" data-id="183574" data-year="19">
+    </div>
+  </div>
+
+
+
+
+  <div class="playercard-chem playercard-chembot">
+    <span class="fut19 icon icon-chem-"></span>
+    <span class="chem-name"></span>
+  </div>
+
+
+  <div class="playercard-attr playercard-attr1">
+    <span class="chembot-value">${this.pac}</span> <span class="playercard-thin">PAC</span>
+    <span class="chembot-delta" data-chembot-field="attr1" data-chembot-base="69"></span>
+  </div>
+  <div class="playercard-attr playercard-attr2">
+    <span class="chembot-value">${this.sho}</span> <span class="playercard-thin">SHO</span>
+    <span class="chembot-delta" data-chembot-field="attr2" data-chembot-base="83"></span>
+  </div>
+  <div class="playercard-attr playercard-attr3">
+    <span class="chembot-value">${this.pas}</span> <span class="playercard-thin">PAS</span>
+    <span class="chembot-delta" data-chembot-field="attr3" data-chembot-base="82"></span>
+  </div>
+  <div class="playercard-attr playercard-attr4">
+    <span class="chembot-value">${this.dri}</span> <span class="playercard-thin">DRI</span>
+    <span class="chembot-delta" data-chembot-field="attr4" data-chembot-base="82"></span>
+  </div>
+  <div class="playercard-attr playercard-attr5">
+    <span class="chembot-value">${this.def}</span> <span class="playercard-thin">DEF</span>
+    <span class="chembot-delta" data-chembot-field="attr5" data-chembot-base="46"></span>
+  </div>
+  <div class="playercard-attr playercard-attr6">
+    <span class="chembot-value">${this.phy}</span> <span class="playercard-thin">PHY</span>
+    <span class="chembot-delta" data-chembot-field="attr6" data-chembot-base="70"></span>
+  </div>
+
+
+
+
+
+</div>
 `;
   }
 
