202fa55d1adc6f94ee425f32eb013793193edb7b
Marius Bongarts <mb709027@fh-muenster.de>
2019-06-29 09:48:12 +0200

    Structure of card

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -7,9 +7,51 @@ class AppComponent extends LitElement {
 
   static styles = css`${unsafeCSS(componentCSS)}`;
 
+  @property()
+  pac: number = 99;
+  @property()
+  sho: number = 99;
+  @property()
+  pas: number = 99;
+  @property()
+  dri: number = 99;
+  @property()
+  def: number = 99;
+  @property()
+  phy: number = 99;
+  @property()
+  name: number = 99;
+  @property()
+  overall: number = 99;
+  @property()
+  playerName: string = 'Messi';
+  @property()
+  imageUrl: string = 'https://thispersondoesnotexist.com/image';
+
   render() {
     return html`
-          <h1>Fifa Card</h1>
+          <div class="card">
+            <div class="cardBody">
+              <span class="overall">${this.overall}</span>
+              <img src=${this.imageUrl}>
+            </div>
+
+            <div class="stats">
+              <div class="playerName">
+                ${this.playerName}
+              </div>
+              <div class="statsLeft">
+                <p>${this.pac} PAC</p>
+                <p>${this.sho} SHO</p>
+                <p>${this.pas} PAS</p>
+              </div>
+              <div class="statsRight">
+                <p>${this.dri} DRI</p>
+                <p>${this.def} DEF</p>
+                <p>${this.phy} PHY</p>
+              </div>
+            </div>
+          </div>
 `;
   }
 
